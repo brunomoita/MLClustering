dfb54a4fd46bc033a5aa250bc2396e6f7ed51ec7
brunomoita <brunosimoes@gmail.com>
2020-07-25 17:13:00 -0400

    Initial commit

---
+++ MLClustering/TestCustData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLClustering
+{
+    static class TestCustData
+    {
+        internal static readonly CustData PredictionObj = new CustData
+        {
+            Male = 300f,
+            Female = 100f,
+            Before2010 = 400f,
+            After2010 = 1400f
+        };
+
+    }
+}
